ec17f36cd4ea50b87275c20e4859da85c03b4500
Branden Torrance <btorrance86@gmail.com>
2022-11-06 01:02:04 -0400

    Add week 8 notes

--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
 // ASSIGNMENT - TABLE OF CONTENTS
 // Place new Assignment name into "label" and assignment URL into "url"
 const listArray = [
-  {
-    label: "Week 01",
-    url: "week-01",
-  },
-  {
-    label: "Week 02",
-    url: "week-02",
-  },
-  {
-    label: "Week 03",
-    url: "week-03",
-  },
-  {
-    label: "Week 04",
-    url: "week-04",
-  },
-  {
-    label: "Week 05",
-    url: "week-05",
-  },
-  {
-    label: "Week 06",
-    url: "week-06",
-  },
-  {
-    label: "Week 07",
-    url: "week-07",
-  },
+    {
+        label: "Week 01",
+        url: "week-01",
+    },
+    {
+        label: "Week 02",
+        url: "week-02",
+    },
+    {
+        label: "Week 03",
+        url: "week-03",
+    },
+    {
+        label: "Week 04",
+        url: "week-04",
+    },
+    {
+        label: "Week 05",
+        url: "week-05",
+    },
+    {
+        label: "Week 06",
+        url: "week-06",
+    },
+    {
+        label: "Week 07",
+        url: "week-07",
+    },
+    {
+        label: "Week 08",
+        url: "week-08",
+    },
 ];
 
 listArray.forEach((listItem) => {
-  const label = listItem.label;
-  const url = listItem.url;
+    const label = listItem.label;
+    const url = listItem.url;
 
-  let ol = document.getElementById("text-insert");
+    let ol = document.getElementById("text-insert");
 
-  ol.innerHTML += `<li><a href=${url}>${label}</a></li>`;
+    ol.innerHTML += `<li><a href=${url}>${label}</a></li>`;
 });
 
 // FOOTER - LAST MODIFIED DATE
